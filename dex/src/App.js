93aa9cd6842623c51a8522bbf032cf877f1fb8a6
milaifeng <2691708883@qq.com>
2025-10-20 22:02:14 +0800

    已完成SWAP界面以及后端获取erc-20代币的美元价格

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import Swap from "./components/Swap";
+import Token from "./components/Tokens";
+import { Route, Routes } from "react-router-dom";
 function App() {
-  return <div className="App">Dex</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="mainWindow">
+        <Routes>
+          <Route path="/" element={<Swap />} />
+          <Route path="/tokens" element={<Token />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,9 +1,162 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Input, Popover, Radio, Modal, message, Divider } from "antd";
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import tokenList from "../tokenList.json";
+import axios from "axios";
 
 function Swap() {
+  const [slippage, setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
+  const [prices, setPrices] = useState(null);
+
+  const handlesSlippageChange = (e) => {
+    setSlippage(e.target.value);
+  };
+  const changeAmount = (e) => {
+    setTokenOneAmount(e.target.value);
+    if (e.target.value && prices) {
+      setTokenTwoAmount((e.target.value * prices.ratio).toFixed(2));
+    } else {
+      setTokenTwoAmount(null);
+    }
+  };
+  const switchTokens = () => {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+    fetchPrices(two.address, one.address);
+  };
+  const openModa = (asset) => {
+    setChangeToken(asset);
+    setIsOpen(true);
+  };
+  const modifyToken = (index) => {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
+    if (changeToken === 1) {
+      setTokenOne(tokenList[index]);
+      fetchPrices(tokenList[index].address, tokenTwo.address);
+    } else {
+      setTokenTwo(tokenList[index]);
+      fetchPrices(tokenOne.address, tokenList[index].address);
+    }
+    setIsOpen(false);
+  };
+  const fetchPrices = async (one, two) => {
+    const res = await axios.get(`http://localhost:8000/tokenPrice`, {
+      params: { addressOne: one, addressTwo: two },
+    });
+    console.log(res.data);
+    setPrices(res.data);
+  };
+  const settings = (
+    <>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handlesSlippageChange}>
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5}>5.0%</Radio.Button>
+        </Radio.Group>
+      </div>
+    </>
+  );
+
+  useEffect(() => {
+    fetchPrices(tokenList[0].address, tokenList[1].address);
+  }, []);
+
   return (
-    <div>Swap</div>
-  )
+    <>
+      <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => {
+          setIsOpen(false);
+        }}
+        title="Select a token"
+      >
+        <div className="modalContent">
+          {tokenList?.map((e, i) => {
+            return (
+              <div
+                className="tokenChoice"
+                key={i}
+                onClick={() => modifyToken(i)}
+              >
+                <img src={e.img} alt={e.ticker} className="tokenLogo" />
+                <div className="tokenChoiceNames">
+                  <div className="tokenName">{e.name}</div>
+                  <div className="tokenTicker">{e.ticker}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
+      <div className="tradeBox">
+        <div className="tradeBoxHeader">
+          <h4>Swap</h4>
+          <Popover
+            content={settings}
+            title="Settings"
+            trigger="click"
+            placement="bottomRight"
+          >
+            <SettingOutlined className="cog" />
+          </Popover>
+        </div>
+        <div className="inputs">
+          <Input
+            placeholder="0"
+            value={tokenOneAmount}
+            onChange={changeAmount}
+          />
+          <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+          <div className="switchButton" onClick={switchTokens}>
+            <ArrowDownOutlined className="switchArrow" />
+          </div>
+          <div
+            className="assetOne"
+            onClick={() => {
+              openModa(1);
+            }}
+          >
+            <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+          <div
+            className="assetTwo"
+            onClick={() => {
+              openModa(2);
+            }}
+          >
+            <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
+        <button className="swapButton" disabled={!tokenOneAmount}>
+          Swap
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Swap
+export default Swap;
